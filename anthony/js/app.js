9cb85036da668ff4b8504067aca633ad25f724a9
Anthony <anthonyjlower@gmail.com>
2017-12-21 16:12:22 -0600

    Add X's to boxes by clicking on them

--- anthony/js/app.js
+++ anthony/js/app.js
@@ -2,10 +2,15 @@ console.log('working')
 
 const gameBoard = $('#board')
 
+
+const addAnX = (evt) => {
+	$(evt.currentTarget).text("X")
+}
+
 for(let i = 1; i <= 9; i++){
 	$newDiv = $('<div>');
 	$newDiv.addClass("square");
-	// $newDiv.on('click', someFunction);
+	$newDiv.on('click', addAnX);
 	gameBoard.append($newDiv);
 }
 
