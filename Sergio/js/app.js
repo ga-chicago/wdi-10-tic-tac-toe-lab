d7f1a97d27cdaf41cf6890bbd004905c7a05684f
sergio <scrummish@gmail.com>
2017-12-21 16:15:22 -0600

    Commit 3: added 3by3 grid

--- Sergio/js/app.js
+++ Sergio/js/app.js
@@ -4,3 +4,10 @@ console.log("working");
 $("#clear-board").on("click",()=>{
 	console.log("clicked");
 })
+
+let $board = $("#board");
+
+for (var i = 0; i < 9; i++) {
+	let $newDiv = $("<div>").addClass("square");
+	$newDiv.appendTo($board);
+}
