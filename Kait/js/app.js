1cc5af4d3052aa351d355c98d3f28035ffc5883f
Kait Mikitin <kait.mikitin@gmail.com>
2017-12-21 16:10:58 -0600

    Commit 3 - added a game board and 3x3 grid

--- Kait/js/app.js
+++ Kait/js/app.js
@@ -3,3 +3,10 @@ console.log('Linked.')
 $('#clear-board').on('click', ()=>{
 	console.log('Clicked!')
 })
+
+let $theBoard = $('#board');
+
+for(let i = 0; i < 9; i++){
+	let $div = $('<div>').attr('class', 'square');
+	$theBoard.append($div);
+}
