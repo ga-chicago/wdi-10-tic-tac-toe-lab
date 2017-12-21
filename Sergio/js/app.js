86ea118b6249a1754eaa82b3f595556049ce9c11
sergio <scrummish@gmail.com>
2017-12-21 16:19:08 -0600

    Commit 4: Added click functionality to each square

--- Sergio/js/app.js
+++ Sergio/js/app.js
@@ -7,7 +7,12 @@ $("#clear-board").on("click",()=>{
 
 let $board = $("#board");
 
+let someFunction = ()=>{
+
+}
+
 for (var i = 0; i < 9; i++) {
 	let $newDiv = $("<div>").addClass("square");
+	$newDiv.on("click", someFunction);
 	$newDiv.appendTo($board);
 }
