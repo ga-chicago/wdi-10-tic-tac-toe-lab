a36dc0f0d6c637a2f120c9968ab73066a53bad4f
Anthony <anthonyjlower@gmail.com>
2017-12-21 16:25:25 -0600

    Game alternates between X's and O's and doesn't allow a player to take an already used square

--- anthony/js/app.js
+++ anthony/js/app.js
@@ -1,16 +1,29 @@
 console.log('working')
 
 const gameBoard = $('#board')
+let isXsTurn = true 
 
 
-const addAnX = (evt) => {
-	$(evt.currentTarget).text("X")
+const addAMark = (evt) => {
+	if($(evt.currentTarget).text() == "")
+		if (isXsTurn) {
+			$(evt.currentTarget).text("X")
+			// console.log(evt.currentTarget)
+			isXsTurn = false
+		} else {
+			// console.log(evt.currentTarget)
+			$(evt.currentTarget).text("O")
+			isXsTurn = true
+		}
+	else{
+		console.log('That spot has been taken, try again.')
+	}	
 }
 
 for(let i = 1; i <= 9; i++){
 	$newDiv = $('<div>');
 	$newDiv.addClass("square");
-	$newDiv.on('click', addAnX);
+	$newDiv.on('click', addAMark);
 	gameBoard.append($newDiv);
 }
 
