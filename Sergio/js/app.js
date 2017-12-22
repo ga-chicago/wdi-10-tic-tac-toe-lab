5b40b4cac0bca0aba7430d8ee3a9c421527615f6
sergio <scrummish@gmail.com>
2017-12-21 21:10:18 -0600

    Added modal to congratulate

--- Sergio/js/app.js
+++ Sergio/js/app.js
@@ -1,6 +1,7 @@
 
-
+let $modal = $("#myModal");
 let $board = $("#board");
+var modal = document.getElementById('myModal');
 let XvsO = true;
 let whoWon = {};
 
@@ -13,51 +14,51 @@ let columnThree = [];
 let downStairs = [];
 let upstairs = [];
 
-// function congratulate(player){
-
-// }
+function congratulate(player){
+	modal.style.display = "block";
+}
 
 function checkIfWon(player){ // Refactor this when you have time too much if else 
 	if (whoWon.cell0 === player && whoWon.cell1 === player && whoWon.cell2 === player){ // Row 1 of 3
 		$("#cell0").addClass("winner");
 		$("#cell1").addClass("winner");
 		$("#cell2").addClass("winner");
-		// return congratulate(player);
+		return congratulate(player);
 	} else if (whoWon.cell3 === player && whoWon.cell4 === player && whoWon.cell5 === player){ // Row 1 of 3
 		$("#cell3").addClass("winner");
 		$("#cell4").addClass("winner");
 		$("#cell5").addClass("winner");
-		// return congratulate(player);
+		return congratulate(player);
 	} else if (whoWon.cell6 === player && whoWon.cell7 === player && whoWon.cell8 === player){ // Row 1 of 3
 		$("#cell3").addClass("winner");
 		$("#cell4").addClass("winner");
 		$("#cell5").addClass("winner");
-		// return congratulate(player);
+		return congratulate(player);
 	} else if (whoWon.cell0 === player && whoWon.cell3 === player && whoWon.cell6 === player){ // Row 1 of 3
 		$("#cell0").addClass("winner");
 		$("#cell3").addClass("winner");
 		$("#cell6").addClass("winner");
-		// return congratulate(player);
+		return congratulate(player);
 	} else if (whoWon.cell1 === player && whoWon.cell4 === player && whoWon.cell7 === player){ // Row 1 of 3
 		$("#cell1").addClass("winner");
 		$("#cell4").addClass("winner");
 		$("#cell7").addClass("winner");
-		// return congratulate(player);
+		return congratulate(player);
 	} else if (whoWon.cell2 === player && whoWon.cell5 === player && whoWon.cell8 === player){ // Row 1 of 3
 		$("#cell2").addClass("winner");
 		$("#cell5").addClass("winner");
 		$("#cell8").addClass("winner");
-		// return congratulate(player);
+		return congratulate(player);
 	} else if (whoWon.cell0 === player && whoWon.cell4 === player && whoWon.cell8 === player){ // Row 1 of 3
 		$("#cell0").addClass("winner");
 		$("#cell4").addClass("winner");
 		$("#cell8").addClass("winner");
-		// return congratulate(player);
+		return congratulate(player);
 	} else if (whoWon.cell2 === player && whoWon.cell4 === player && whoWon.cell6 === player){ // Row 1 of 3
 		$("#cell2").addClass("winner");
 		$("#cell4").addClass("winner");
 		$("#cell6").addClass("winner");
-		// return congratulate(player);
+		return congratulate(player);
 	}
 }
 
@@ -93,3 +94,19 @@ $("#clear-board").on("click", ()=>{
 		$("#cell"+ i).removeClass("winner");
 	}
 })
+
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+
+
+
+
+
+
+
