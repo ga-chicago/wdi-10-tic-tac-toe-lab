b2a5838af1f86fd77086cf899b7b58010fcb00bd
Anthony <anthonyjlower@gmail.com>
2017-12-22 12:28:31 -0600

    Game will announce winner for either player

--- anthony/js/app.js
+++ anthony/js/app.js
@@ -12,24 +12,26 @@ const addAMark = (evt) => {
 	if($(evt.currentTarget).text() == "")
 		if (isXsTurn) {
 			$(evt.currentTarget).text("X");
-			xPlays.push($(evt.currentTarget).attr('id'));
+			// xPlays.push($(evt.currentTarget).attr('id'));
 			// console.log($(evt.currentTarget).attr('id'))
 
 			// console.log(evt.currentTarget)
+			checkForWinner(player);
 			isXsTurn = false;
-			player = "Y"
+			player = "O"
 			
 		} else {
 			// console.log(evt.currentTarget)
 			$(evt.currentTarget).text("O");
-			oPlays.push($(evt.currentTarget).attr('id'));			
+			oPlays.push($(evt.currentTarget).attr('id'));
+			checkForWinner(player);
 			isXsTurn = true;
-			Player = "X"
+			player = "X"
 		}
 	else{
 		console.log('That spot has been taken, try again.');
 	}
-	checkForWinner()	
+		
 }
 
 const resetGame = () => {
@@ -38,35 +40,35 @@ const resetGame = () => {
 
 const checkForWinner = (player) => {
 	//Top Row across
-	if ($('#spot-1').text() === "X" && $('#spot-2').text() === "X" && $('#spot-3').text() === "X") {
+	if ($('#spot-1').text() === player && $('#spot-2').text() === player && $('#spot-3').text() === player) {
 		console.log("Winner")
 	
 	// Middle Row Across
-	} else if ($('#spot-4').text() === "X" && $('#spot-5').text() === "X" && $('#spot-6').text() === "X") {
+	} else if ($('#spot-4').text() === player && $('#spot-5').text() === player && $('#spot-6').text() === player) {
 		console.log("Winner")
 	
 	//Bottom Row Across
-	} else if ($('#spot-7').text() === "X" && $('#spot-8').text() === "X" && $('#spot-9').text() === "X") {
+	} else if ($('#spot-7').text() === player && $('#spot-8').text() === player && $('#spot-9').text() === player) {
 		console.log("Winner")
 	
 	//Left column
-	} else if ($('#spot-1').text() === "X" && $('#spot-4').text() === "X" && $('#spot-7').text() === "X") {
+	} else if ($('#spot-1').text() === player && $('#spot-4').text() === player && $('#spot-7').text() === player) {
 		console.log("Winner")
 	
 	//middle column
-	} else if ($('#spot-2').text() === "X" && $('#spot-5').text() === "X" && $('#spot-8').text() === "X") {
+	} else if ($('#spot-2').text() === player && $('#spot-5').text() === player && $('#spot-8').text() === player) {
 		console.log("Winner")
 	
 	//right column
-	} else if ($('#spot-3').text() === "X" && $('#spot-6').text() === "X" && $('#spot-9').text() === "X") {
+	} else if ($('#spot-3').text() === player && $('#spot-6').text() === player && $('#spot-9').text() === player) {
 		console.log("Winner")
 	
 	//top left to bottom right diag
-	} else if ($('#spot-1').text() === "X" && $('#spot-5').text() === "X" && $('#spot-9').text() === "X") {
+	} else if ($('#spot-1').text() === player && $('#spot-5').text() === player && $('#spot-9').text() === player) {
 		console.log("Winner")
 	
 	//top right to bottomw left diag
-	} else if ($('#spot-3').text() === "X" && $('#spot-5').text() === "X" && $('#spot-7').text() === "X") {
+	} else if ($('#spot-3').text() === player && $('#spot-5').text() === player && $('#spot-7').text() === player) {
 		console.log("Winner")
 	}else {
 		console.log("Keep Playing")
