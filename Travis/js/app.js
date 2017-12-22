f797b377b51d00328d5a42ecc475e0e2e786cac5
Travis1282 <tmail20@gmail.com>
2017-12-21 19:22:20 -0600

    got the x and the o alternating

--- Travis/js/app.js
+++ Travis/js/app.js
@@ -1,12 +1,26 @@
 let board = $('#board')
 let	$div = $('#square');
+let whosTurn = 'X';
 
 
 for(let i =0; i<9; i++){
 	//generate div dynamically 
 	//save to a variable
 	const $div = $('<div>');
-	$div.text('X');
+	
+	
 	$div.addClass('square');
 	$div.appendTo('#board');
-}
+
+	$div.on('click', ()=>{
+		if(whosTurn == 'X'){
+		$div.text('X');
+		whosTurn = 'O';
+		console.log(whosTurn)
+	}else {
+		$div.text('O');
+		whosTurn = 'X';
+		}
+	})
+
+}	
