ec12a7fd8192174edc0b318140a268bbbc380049
Kait Mikitin <kait.mikitin@gmail.com>
2017-12-23 11:37:58 -0600

    Commit 13 - created game logic, logic is untested

--- Kait/js/app.js
+++ Kait/js/app.js
@@ -7,34 +7,79 @@ let yScore = 0;
 // MAX ROUND SHOULD BE 3
 let round = 0;
 
-function showX(e){
+let xSquares = [];
+let oSquares = [];
+
+function showX(evt){
 	// console.log(e.currentTarget)
 	if (toggle === true) {
-		$(e.currentTarget).toggleClass('o-class')
-		$(e.currentTarget).text('O')
+		$(evt.currentTarget).toggleClass('o-class')
+		$(evt.currentTarget).text('O')
 		toggle = false;
 	} else if (toggle === false){
-		$(e.currentTarget).text('X')
+		$(evt.currentTarget).text('X')
 		toggle = true;
 	}
 	console.log(this)
 }
+
+
 
 function setBoard() {
 	for(let i = 0; i < 9; i++){
 		let $div = $('<div class="square"></div>').attr('id', [i]);
 		$theBoard.append($div);
 		// one method as apposed to on method! Only allows the user to click once.
-		$div.one('click', showX)
+		$div.one('click', showX);
 	}
 }
+
+function checkWinner(){
+	if($('#0').text() === 'X' && $('#4').text() === 'X' && $('#8') === 'X'){
+		console.log('X wins')
+	} else if ($('#0').text() === 'O' && $('#4').text() === 'O' && $('#8') === 'O'){
+		console.log('O wins')
+	} else if ($('#2').text() === 'X' && $('#4').text() === 'X' && $('#6') === 'X'){
+		console.log('X wins')
+	} else if ($('#2').text() === 'O' && $('#4').text() === 'O' && $('#6') === 'O'){
+		console.log('O wins')
+	} else if ($('#0').text() === 'X' && $('#1').text() === 'X' && $('#2') === 'X'){
+		console.log('X wins')
+	} else if ($('#0').text() === 'O' && $('#1').text() === 'O' && $('#2') === 'O'){
+		console.log('O wins')
+	} else if ($('#3').text() === 'X' && $('#4').text() === 'X' && $('#5') === 'X'){
+		console.log('X wins')	
+	} else if ($('#3').text() === 'O' && $('#4').text() === 'O' && $('#5') === 'O'){
+		console.log('O wins')
+	} else if ($('#6').text() === 'X' && $('#7').text() === 'X' && $('#8') === 'X'){
+		console.log('X wins')
+	} else if ($('#6').text() === 'O' && $('#7').text() === 'O' && $('#8') === 'O'){
+		console.log('O wins')
+	} else if ($('#2').text() === 'X' && $('#5').text() === 'X' && $('#8') === 'X'){
+		console.log('X wins')
+	} else if ($('#2').text() === 'O' && $('#5').text() === 'O' && $('#8') === 'O'){
+		console.log('O wins')	
+	}  else if ($('#1').text() === 'X' && $('#4').text() === 'X' && $('#7') === 'X'){
+		console.log('X wins')
+	}							
+}
+
+function gameOver(){
+	if (round === 3){
+		alert('Game over!');
+	} else if (xScore === 2){
+		alert('X won!');
+	} else if (yScore === 2){
+		alert('Y won!');
+	}
+}
+// gameOver()
 
 $('#start-button').on('click', ()=>{
 	$('#start-button').remove()
 	setBoard()
 })
 
-// setBoard()
 
 $('#next-round').on('click', ()=>{
 	// console.log('Clicked!')
@@ -42,6 +87,7 @@ $('#next-round').on('click', ()=>{
 	$('.square').remove()
 	round++
 	setBoard()
+	gameOver()
 })
 
 $('#reset').on('click', ()=>{
@@ -52,7 +98,7 @@ $('#reset').on('click', ()=>{
 })
 
 
-console.log($('.square').attr('id'))
+// console.log($('.square').attr('id'))
 /* GAME LOGIC:
 •if three letters appear in a row game over
 •if three letters appear in a row use a modal to notify player
